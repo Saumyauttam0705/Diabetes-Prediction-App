2cf659aa4d1d7e77f93b169dd92ed76d81e6548a
Saumya Uttam <saumyauttamknp54@gmail.com>
2025-10-27 19:24:14 +0530

    Add files via upload

---
+++ Diabetes Prediction web app.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 27 19:04:24 2025
+
+@author: LENOVO
+"""
+
+import numpy as np
+import pickle
+import streamlit as st
+
+# loading the saved model
+loaded_model = pickle.load(open('C:/Users/LENOVO/OneDrive/Desktop/Deploying Machine Model/trained_model.sav', 'rb'))
+
+#creating a function for prediction
+def diabetes_prediction(input_data):
+    
+    
+    # changing the input_data to a numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    if (prediction[0] == 0):
+        return 'The person is not diabetic'
+    else:
+        return 'The person is diabetic'
+    
+   
+def main():
+    
+    
+    
+    # giving a title
+    st.title('Diabetes Prediction Web App')
+    
+    
+    #getting the input data from the user
+    
+    Pregnancies = st.text_input('Number of Pregnancies')
+    Glucose = st.text_input('Glucose Value')
+    BloodPressure = st.text_input('Blood Pressure Value')
+    SkinThickness = st.text_input('Skin Thickness Value')
+    Insulin = st.text_input('Insulin Value')
+    BMI = st.text_input('BMI Value')
+    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function Value')
+    Age = st.text_input('Age of the person')
+    
+    
+    # code for the prediction
+    diagnosis = ''
+    
+    # creating a button for prediction
+    if st.button("Diabetes Test Result"):
+        diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
+        
+        
+    st.success(diagnosis)
+    
+    
+if __name__ == '__main__':
+    main()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
